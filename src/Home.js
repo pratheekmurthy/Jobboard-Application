39a6f870d3c138b9290032980de8abef7c93f090
pratheekmurthy <pratheekkmurthy516@gmail.com>
2020-12-25 20:13:35 +0530

    carousel

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,52 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
+import "./App.css"
 import Carousel from 'react-bootstrap/Carousel'
 
 
-
-
-
 const Homepage =(props)=>{
+    
     return (<div>
       <h1></h1>
-     
+      <Carousel >
+  <Carousel.Item >
+  {/* <span aria-hidden="false" className="carousel-control-next-icon" /> */}
+    <img
+      className="d-block w-100"
+      src="/images/one.jpeg"
+      alt="First slide"
+    />
+    <Carousel.Caption >
+      <p>Be curious. Use data. Leverage imagination. Be an expert. Be an enthusiast. Be authentic. Know your competition.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/images/3.jpeg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <p>Your human talent is your most important talent..</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/images/3.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <p>Hire people who are smarter than you are—whose talents surpass yours—and give them opportunities for growth. It’s the smart thing to do and it is a sign of high personal humility. </p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
     </div>
+     
     )
 }
 
 export default Homepage
 
-
-//  <Carousel>
-//           <Carousel.item>
-//             <img  className="d-block w-100" src={require("./images/one.jpg")} alt="one.jpg" style={{heiht : "100vh"}}/>
-//             <Carousel.Caption>
-//                 <h3>1st image</h3>
-//             </Carousel.Caption>
-//           </Carousel.item>
-//           <Carousel.item>
-//             <img  className="d-block w-100" src={require("./images/2.jpeg")} alt="one.jpg" style={{heiht : "100vh"}}/>
-//             <Carousel.Caption>
-//                 <h3>1st image</h3>
-//             </Carousel.Caption>
-//           </Carousel.item>
-//           <Carousel.item>
-//             <img  className="d-block w-100" src={require("./images/3.jpg")} alt="one.jpg" style={{heiht : "100vh"}}/>
-//             <Carousel.Caption>
-//                 <h3>1st image</h3>
-//             </Carousel.Caption>
-//           </Carousel.item>
-//     </Carousel>
